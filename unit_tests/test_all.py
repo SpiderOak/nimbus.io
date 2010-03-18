a39ea069077d5177f1e4425d6f772df6a7b2489c
dougfort <dougfort@sa10.com>
2010-03-18 10:50:18 -0400

    unit_tests: use the right path in test_all

--- unit_tests/test_all.py
+++ unit_tests/test_all.py
@@ -12,12 +12,24 @@ _test_re = re.compile("test_.+?\.py$", re.IGNORECASE)
 _filename_to_module = lambda f: os.path.splitext(f)[0]
 _load = unittest.defaultTestLoader.loadTestsFromModule  
 
+def _find_in_python_path(path_segments):
+    """
+    Find the path to a given item somewhere in PYTHONPATH
+    """
+    python_path = os.environ["PYTHONPATH"]
+    for work_path in python_path.split(os.pathsep):
+        test_path = os.path.join(work_path, path_segments)
+        if os.path.isdir(unicode(test_path)):
+            return test_path
+
+    raise RuntimeError("Can't find %r in PYTHONPATH" % path_segments)
+
 def _regression_test():
     """
     find all unit tests in a directory.
     This is adapted from Mark Pilgrim's 'Dive Into Python'
-    """
-    path = "/home/dougfort/spideroak_diyapi/unit_tests"
+    """    
+    path = _find_in_python_path("unit_tests")
     files = os.listdir(path)                               
     files = filter(_test_re.search, files)                     
     module_names = map(_filename_to_module, files)         

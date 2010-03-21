fd8dc8b8e76107b5ba198a6c5773921e71c480ac
dougfort <dougfort@sa10.com>
2010-03-21 15:46:05 -0400

    archive_key_entire message: added marshalling and unmarshalling code

--- messages/archive_key_entire.py
+++ messages/archive_key_entire.py
@@ -4,7 +4,34 @@ archive_key_entire.py
 
 ArchiveKeyEntire message
 """
+from collections import namedtuple
 import struct
+
+_header_tuple = namedtuple("Header", [ 
+    "request_id", 
+    "avatar_id", 
+    "reply_exchange_size", 
+    "reply_routing_key_size", 
+    "key_size",
+    "timestamp",
+    "segment_number",
+    "adler32",
+    "md5"
+])
+
+# 32s - request-id 32 char hex uuid
+# Q   - avatar_id 
+# I   - reply-exchange size
+# I   - reply-key size
+# I   - key size
+# d   - timestamp
+# B   - segment_number
+# I   - adler32
+# 16s - md5
+_header_format = "!32sQIIIdBI16s"
+_header_size = struct.calcsize(_header_format)
+
+_string_format = "%ds"
 
 class ArchiveKeyEntire(object):
     """
@@ -16,23 +43,23 @@ class ArchiveKeyEntire(object):
     def __init__( 
         self, 
         request_id, 
+        avatar_id, 
         reply_exchange,
         reply_routing_key,
-        avatar_id, 
         key, 
         timestamp, 
-        segment, 
+        segment_number, 
         adler32, 
         md5, 
         content
     ):
         self.request_id = request_id
+        self.avatar_id = avatar_id
         self.reply_exchange = reply_exchange
         self.reply_routing_key = reply_routing_key
-        self.avatar_id = avatar_id
         self.key = key
-        self.begin_timestamp = timestamp
-        self.segment = segment
+        self.timestamp = timestamp
+        self.segment_number = segment_number
         self.adler32 = adler32
         self.md5 = md5
         self.content = content
@@ -40,8 +67,73 @@ class ArchiveKeyEntire(object):
     @classmethod
     def unmarshall(cls, data):
         """return a ArchiveKeyEntire message"""
+        pos = 0
+        header = _header_tuple._make(struct.unpack(
+            _header_format, data[pos:pos+_header_size]
+        ))
+        pos += _header_size
+        (reply_exchange, ) = struct.unpack(
+            _string_format % (header.reply_exchange_size, ),
+            data[pos:pos+header.reply_exchange_size]
+        )
+        pos += header.reply_exchange_size
+        (reply_routing_key, ) = struct.unpack(
+            _string_format % (header.reply_routing_key_size, ), 
+            data[pos:pos+header.reply_routing_key_size]
+        )
+        pos += header.reply_routing_key_size
+        (key, ) = struct.unpack(
+            _string_format % (header.key_size, ), 
+            data[pos:pos+header.key_size]
+        )
+        pos += header.key_size
+        content = data[pos:]
+        return ArchiveKeyEntire(
+            header.request_id, 
+            header.avatar_id, 
+            reply_exchange,
+            reply_routing_key,
+            key, 
+            header.timestamp, 
+            header.segment_number, 
+            header.adler32, 
+            header.md5, 
+            content
+        )
 
     def marshall(self):
         """return a data string suitable for transmission"""
+        reply_exchange_size = len(self.reply_exchange)
+        reply_routing_key_size = len(self.reply_routing_key)
+        key_size = len(self.key)
+        header = struct.pack(
+            _header_format,
+            self.request_id,
+            self.avatar_id,
+            reply_exchange_size, 
+            reply_routing_key_size, 
+            key_size, 
+            self.timestamp,
+            self.segment_number,
+            self.adler32,
+            self.md5
+        )
 
+        packed_reply_exchange = struct.pack(
+            _string_format % (reply_exchange_size, ), self.reply_exchange
+        )
+        packed_reply_routing_key = struct.pack(
+            _string_format % (reply_routing_key_size, ), 
+            self.reply_routing_key
+        )
+        packed_key = struct.pack(_string_format % (key_size, ), self.key)
+        return "".join(
+            [
+                header,
+                packed_reply_exchange,
+                packed_reply_routing_key,
+                packed_key,
+                self.content
+            ]
+        )
 
--- unit_tests/test_messages.py
+++ unit_tests/test_messages.py
@@ -4,14 +4,21 @@ test_messages.py
 
 test AMQP Messages
 """
+from hashlib import md5
 import logging
+import random
 import time
 import unittest
 import uuid
+from zlib import adler32
 
 from diyapi_database_server import database_content
 from messages.database_key_insert import DatabaseKeyInsert
 from messages.database_key_insert_reply import DatabaseKeyInsertReply
+from messages.archive_key_entire import ArchiveKeyEntire
+
+def _random_string(size):
+    return "".join([chr(random.randrange(255)) for _ in xrange(size)])
 
 class TestMessages(unittest.TestCase):
     """test AMQP Messages"""
@@ -73,6 +80,49 @@ class TestMessages(unittest.TestCase):
         self.assertEqual(
             unmarshalled_message.previous_size, original_previous_size
         )
+
+    def test_archive_key_entire(self):
+        """test ArchiveKeyEntire"""
+        original_content = _random_string(64 * 1024) 
+        original_request_id = uuid.uuid1().hex
+        original_avatar_id = 1001
+        original_reply_exchange = "reply-exchange"
+        original_reply_routing_key = "reply.routing-key"
+        original_key  = "abcdefghijk"
+        original_timestamp = time.time()
+        original_segment_number = 3
+        original_adler32 = adler32(original_content)
+        original_md5 = md5(original_content).digest()
+        message = ArchiveKeyEntire(
+            original_request_id,
+            original_avatar_id,
+            original_reply_exchange,
+            original_reply_routing_key,
+            original_key, 
+            original_timestamp,
+            original_segment_number,
+            original_adler32,
+            original_md5,
+            original_content
+        )
+        marshalled_message = message.marshall()
+        unmarshalled_message = ArchiveKeyEntire.unmarshall(marshalled_message)
+        self.assertEqual(unmarshalled_message.request_id, original_request_id)
+        self.assertEqual(unmarshalled_message.avatar_id, original_avatar_id)
+        self.assertEqual(
+            unmarshalled_message.reply_exchange, original_reply_exchange
+        )
+        self.assertEqual(
+            unmarshalled_message.reply_routing_key, original_reply_routing_key
+        )
+        self.assertEqual(unmarshalled_message.key, original_key)
+        self.assertEqual(unmarshalled_message.timestamp, original_timestamp)
+        self.assertEqual(
+            unmarshalled_message.segment_number, original_segment_number
+        )
+        self.assertEqual(unmarshalled_message.adler32, original_adler32)
+        self.assertEqual(unmarshalled_message.md5, original_md5)
+        self.assertEqual(unmarshalled_message.content, original_content)
 
 if __name__ == "__main__":
     unittest.main()

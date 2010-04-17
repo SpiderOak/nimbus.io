444b7c38d0e08990e992e0ce72e2cce6a8ac3310
dougfort <dougfort@sa10.com>
2010-04-17 16:49:00 -0400

    database_content: add format_version, userid, groupid, permissions

--- diyapi_database_server/database_content.py
+++ diyapi_database_server/database_content.py
@@ -9,6 +9,7 @@ import struct
 
 from diyapi_tools.marshalling import marshall_string, unmarshall_string
 
+# I - format-version
 # ? - is_tombstone
 # d - timestamp
 # I - version_number
@@ -18,11 +19,16 @@ from diyapi_tools.marshalling import marshall_string, unmarshall_string
 # Q - total_size
 # L - adler32
 #16s- md5 
-_content_template = "?dIBIIQL16s"
+# H - userid
+# H - groupid
+# H - permissions
+_content_template = "I?dIBIIQL16sHHH"
 _content_template_size = struct.calcsize(_content_template)
+_current_format_version = 1
 
-factory =  namedtuple(
+create_content =  namedtuple(
     "DatabaseContent", [
+        "format_version",
         "is_tombstone", 
         "timestamp", 
         "version_number",
@@ -32,13 +38,35 @@ factory =  namedtuple(
         "total_size", 
         "adler32",
         "md5",
+        "userid",
+        "groupid",
+        "permissions",
         "file_name",
     ]
 )
 
+def factory(**kwdargs):
+    return create_content(
+        format_version = _current_format_version,
+        is_tombstone = kwdargs["is_tombstone"], 
+        timestamp = kwdargs["timestamp"], 
+        version_number = kwdargs["version_number"],
+        segment_number = kwdargs["segment_number"], 
+        segment_count = kwdargs["segment_count"],
+        segment_size = kwdargs["segment_size"],
+        total_size = kwdargs["total_size"], 
+        adler32 = kwdargs["adler32"],
+        md5 = kwdargs["md5"],
+        file_name = kwdargs["file_name"],
+        userid = 0,
+        groupid = 0,
+        permissions = 0
+    )
+
 def create_tombstone(timestamp, version_number, segment_number):
     """create a 'tombstone' database entry with a specified timestamp"""
-    return factory(
+    return create_content(
+        format_version = _current_format_version,
         is_tombstone = True, 
         timestamp = timestamp, 
         version_number = version_number,
@@ -49,6 +77,9 @@ def create_tombstone(timestamp, version_number, segment_number):
         adler32 = 0,
         md5 = "",
         file_name = "",
+        userid = 0,
+        groupid = 0,
+        permissions = 0
     )
 
 def marshall(content):
@@ -58,6 +89,7 @@ def marshall(content):
     """
     packed_content = struct.pack(
         _content_template,
+        content.format_version & 0xFFFF,
         content.is_tombstone & 0xFF, 
         content.timestamp, 
         content.version_number & 0xFFFFFFFF, 
@@ -66,7 +98,10 @@ def marshall(content):
         content.segment_size & 0xFFFFFFFF, 
         content.total_size, 
         content.adler32 & 0xFFFFFFFF,
-        content.md5
+        content.md5,
+        content.userid & 0xFFFF,
+        content.groupid & 0xFFFF,
+        content.permissions & 0xFFFF
     )
     packed_file_name = marshall_string(content.file_name)
     return "".join([packed_content, packed_file_name, ])
@@ -80,9 +115,10 @@ def unmarshall(data, pos):
     content = struct.unpack(
         _content_template, data[pos:pos+_content_template_size]
     )
+    assert content[0] == _current_format_version, content
     pos += _content_template_size
     (file_name, pos, ) = unmarshall_string(data, pos)
     total_content = list(content)
     total_content.append(file_name)
-    return (factory._make(total_content), pos)
+    return (create_content._make(total_content), pos)
 

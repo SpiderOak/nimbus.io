0f5ef768f2dbf6b15f1bfc8f84cf3d6a472e63dc
David Hain <dhain@zognot.org>
2010-04-23 13:04:18 -0700

    web_server: some test cleanup

--- unit_tests/web_server/test_sql_authenticator.py
+++ unit_tests/web_server/test_sql_authenticator.py
@@ -16,11 +16,25 @@ from unit_tests.web_server import util
 from diyapi_web_server.sql_authenticator import SqlAuthenticator
 
 
+_test_username = 'test_username'
+_test_key_id = 1001
+_test_key = 'deadbeef'
+
+
 class TestSqlAuthenticator(unittest.TestCase):
     """test diyapi_web_server/sql_authenticator.py"""
     def setUp(self):
         self.req = Request.blank('/')
         self.connection = util.MockSqlConnection()
+        self.connection.cursor().rows = {
+            ('select key '
+             'from diy_key '
+             'where key_id=%s', (_test_key_id,)): [(_test_key,)],
+            ('select key_id '
+             'from diy_user '
+             'join diy_user_key using (user_id) '
+             'where username=%s', (_test_username,)): [(_test_key_id,)],
+        }
         self.authenticator = SqlAuthenticator(self.connection)
         self._real_time = time.time
         time.time = util.fake_time
@@ -36,93 +50,86 @@ class TestSqlAuthenticator(unittest.TestCase):
         self.assertFalse(self.authenticator.authenticate(self.req))
 
     def test_succeeds(self):
-        key_id = 1001
-        key = 'deadbeef'
         timestamp = int(util.fake_time())
-        self.connection.cursor().rows[
-            ('select key from diy_key where key_id=%s', (key_id,))
-        ] = [(key,)]
         self.req.headers['x-diyapi-timestamp'] = str(timestamp)
         string_to_sign = '\n'.join((
             self.req.method,
             self.req.headers['x-diyapi-timestamp'],
         ))
-        signature = hmac.new(key, string_to_sign, hashlib.sha256).hexdigest()
-        self.req.authorization = 'DIYAPI %d:%s' % (key_id, signature)
+        signature = hmac.new(
+            _test_key,
+            string_to_sign,
+            hashlib.sha256
+        ).hexdigest()
+        self.req.authorization = 'DIYAPI %d:%s' % (_test_key_id, signature)
         self.assertTrue(self.authenticator.authenticate(self.req))
-        self.assertEqual(self.req.remote_user, key_id)
+        self.assertEqual(self.req.remote_user, _test_key_id)
 
     def test_fails_for_nonexistent_key_id(self):
-        key_id = 1001
-        key = 'deadbeef'
         timestamp = int(util.fake_time())
         self.req.headers['x-diyapi-timestamp'] = str(timestamp)
         string_to_sign = '\n'.join((
             self.req.method,
             self.req.headers['x-diyapi-timestamp'],
         ))
-        signature = hmac.new(key, string_to_sign, hashlib.sha256).hexdigest()
-        self.req.authorization = 'DIYAPI %d:%s' % (key_id, signature)
+        signature = hmac.new(
+            _test_key,
+            string_to_sign,
+            hashlib.sha256
+        ).hexdigest()
+        self.req.authorization = 'DIYAPI %d:%s' % (_test_key_id + 1, signature)
         self.assertFalse(self.authenticator.authenticate(self.req))
 
     def test_succeeds_if_timestamp_within_10_minutes(self):
-        key_id = 1001
-        key = 'deadbeef'
         timestamp = int(util.fake_time()) + 10 * 60
-        self.connection.cursor().rows[
-            ('select key from diy_key where key_id=%s', (key_id,))
-        ] = [(key,)]
         self.req.headers['x-diyapi-timestamp'] = str(timestamp)
         string_to_sign = '\n'.join((
             self.req.method,
             self.req.headers['x-diyapi-timestamp'],
         ))
-        signature = hmac.new(key, string_to_sign, hashlib.sha256).hexdigest()
-        self.req.authorization = 'DIYAPI %d:%s' % (key_id, signature)
+        signature = hmac.new(
+            _test_key,
+            string_to_sign,
+            hashlib.sha256
+        ).hexdigest()
+        self.req.authorization = 'DIYAPI %d:%s' % (_test_key_id, signature)
         self.assertTrue(self.authenticator.authenticate(self.req))
-        self.assertEqual(self.req.remote_user, key_id)
+        self.assertEqual(self.req.remote_user, _test_key_id)
 
     def test_fails_if_missing_timestamp(self):
-        key_id = 1001
-        key = 'deadbeef'
         timestamp = int(util.fake_time())
-        self.connection.cursor().rows[
-            ('select key from diy_key where key_id=%s', (key_id,))
-        ] = [(key,)]
         string_to_sign = '\n'.join((
             self.req.method,
             str(timestamp),
         ))
-        signature = hmac.new(key, string_to_sign, hashlib.sha256).hexdigest()
-        self.req.authorization = 'DIYAPI %d:%s' % (key_id, signature)
+        signature = hmac.new(
+            _test_key,
+            string_to_sign,
+            hashlib.sha256
+        ).hexdigest()
+        self.req.authorization = 'DIYAPI %d:%s' % (_test_key_id, signature)
         self.assertFalse(self.authenticator.authenticate(self.req))
 
     def test_fails_if_timestamp_skewed(self):
-        key_id = 1001
-        key = 'deadbeef'
         timestamp = int(util.fake_time()) + 10 * 60 + 1
-        self.connection.cursor().rows[
-            ('select key from diy_key where key_id=%s', (key_id,))
-        ] = [(key,)]
         self.req.headers['x-diyapi-timestamp'] = str(timestamp)
         string_to_sign = '\n'.join((
             self.req.method,
             self.req.headers['x-diyapi-timestamp'],
         ))
-        signature = hmac.new(key, string_to_sign, hashlib.sha256).hexdigest()
-        self.req.authorization = 'DIYAPI %d:%s' % (key_id, signature)
+        signature = hmac.new(
+            _test_key,
+            string_to_sign,
+            hashlib.sha256
+        ).hexdigest()
+        self.req.authorization = 'DIYAPI %d:%s' % (_test_key_id, signature)
         self.assertFalse(self.authenticator.authenticate(self.req))
 
     def test_fails_if_signature_mismatched(self):
-        key_id = 1001
-        key = 'deadbeef'
         timestamp = int(util.fake_time())
-        self.connection.cursor().rows[
-            ('select key from diy_key where key_id=%s', (key_id,))
-        ] = [(key,)]
         self.req.headers['x-diyapi-timestamp'] = str(timestamp)
         signature = 'bogussignature'
-        self.req.authorization = 'DIYAPI %d:%s' % (key_id, signature)
+        self.req.authorization = 'DIYAPI %d:%s' % (_test_key_id, signature)
         self.assertFalse(self.authenticator.authenticate(self.req))
 
 

89760e0e295eb5a5e073e2a95f25eecb4392dc19
dougfort <dougfort@sa10.com>
2010-06-17 07:25:10 -0400

    reconstructor: stub of unit test

---
+++ unit_tests/test_reconstructor.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+test_reconstructor.py
+
+test the reconstructor
+this ends up being a simulator for almost the full system
+"""
+import datetime
+import logging
+import os
+import os.path
+import shutil
+import time
+import unittest
+import uuid
+
+from diyapi_tools.standard_logging import initialize_logging
+
+from unit_tests.util import generate_key
+
+_log_path = "/var/log/pandora/test_reconstructor.log"
+
+class TestReconstructor(unittest.TestCase):
+    """test message handling in reconstructor"""
+
+    def setUp(self):
+        self.tearDown()
+
+    def tearDown(self):
+        pass
+
+    def test_valid_avatar(self):
+        """test an avatar that has no errors"""
+        avatar_id = 1001
+
+if __name__ == "__main__":
+    unittest.main()
+

29600bba09a7922fe83836d816c0fa38b5c184b9
dougfort <dougfort@sa10.com>
2010-08-31 14:29:32 -0400

    add a rabbitmqctl script fro the test cluster

---
+++ scripts/diy_rabbitmqctl.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export TOOL="/opt/so2.6"
+source ${TOOL}/bin/env.sh
+export SPIDEROAK_DIY="${HOME}/spideroak_diyapi"
+export PYTHONPATH="${SPIDEROAK_DIY}"
+
+export SPIDEROAK_MULTI_NODE_NAME="node-sim-01"
+export RABBITMQ_NODENAME="${SPIDEROAK_MULTI_NODE_NAME}"
+export RABBITMQ_NODE_IP_ADDRESS="127.0.0.1"
+export RABBITMQ_NODE_PORT="6001"
+
+sudo rabbitmqctl list_queues -p spideroak_vhost

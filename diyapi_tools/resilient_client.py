1db5844a13ffee42582e3014a1ed580f467f78ad
dougfort <dougfort@dougfort.com>
2011-04-26 13:46:07 -0400

    start on resilient client and server

---
+++ diyapi_tools/resilient_client.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+resilient_client.py
+
+a class that manages a zeromq XREQ socket as a client,
+to a resilient server
+"""
+from collections import deque, namedtuple
+import logging
+import time
+import uuid
+
+import zmq
+
+# our internal message format
+_message_format = namedtuple("Message", "control body")
+
+class ResilientClient(object):
+    """
+    a class that manages a zeromq XREQ socket as a client
+    """
+    def __init__(self, context, server_address, client_tag, client_address):
+        self._log = logging.getLogger("ResilientClient-%s" % (
+            server_address, 
+        ))
+
+        self._xreq_socket = context.socket(zmq.XREQ)
+        self._log.debug("connecting")
+        self._xreq_socket.connect(server_address)
+
+        self._send_queue = deque()
+
+        self._client_tag = client_tag
+        self._client_address = client_address
+
+        self._pending_ack = None
+        self._pending_ack_start_time = None
+
+    def register(self, pollster):
+        pollster.register_read(
+            self._xreq_socket, self._pollster_callback
+        )
+
+    def unregister(self, pollster):
+        pollster.unregister(self._xreq_socket)
+
+    def close(self):
+        self._xreq_socket.close()
+
+    def queue_message_for_send(self, message_control, data=None):
+        if self._pending_ack is None:
+            self._send_message(
+                _message_format(control=message_control, body=data)
+            )
+        else:
+            self._send_queue.append(
+                _message_format(control=message_control, body=data)
+            )
+
+    def _send_handshake(self):
+        assert self._pending_ack is None
+        message = {
+            "message-type"      : "resilient-server-handshake",
+            "request-id"        : uuid.uuid1().hex,
+            "client-tag"        : self._client_tag,
+            "client-address"    : self._client_address,
+        }
+        self._send_message(_message_format(control=message, body=None))
+
+    def _pollster_callback(self, _active_socket, readable, writable):
+        message = self._receive_message()     
+
+        # if we get None, that means the socket would have blocked
+        # go back and wait for more
+        if message is None:
+            return None
+
+        assert self._pending_ack is not None
+        if message["request-id"] != self._pending_ack:
+            self._log.error("unknown ack %s" %(message, ))
+            return
+
+        self._pending_ack = None
+        self._pending_ack_start_time = None
+
+        try:
+            message_to_send = self._send_queue.popleft()
+        except IndexError:
+            return
+
+        self._send_message(message_to_send)
+            
+    def _send_message(self, message):
+        self._log.info("sending message: %s" % (message.control, ))
+        if message.body is not None:
+            self._xreq_socket.send_json(message.control, zmq.SNDMORE)
+            if type(message.body) not in [list, tuple, ]:
+                message = message._replace(body=[message.body, ])
+            for segment in message.body[:-1]:
+                self._xreq_socket.send(segment, zmq.SNDMORE)
+            self._xreq_socket.send(message.body[-1])
+        else:
+            self._xreq_socket.send_json(message.control)
+
+        self._pending_ack = message.control["request-id"]
+        self._pending_ack_start_time = time.time()
+
+    def _receive_message(self):
+        # we should only be receiving ack, so we don't
+        # check for multipart messages
+        try:
+            return self._xreq_socket.recv_json(zmq.NOBLOCK)
+        except zmq.ZMQError, instance:
+            if instance.errno == zmq.EAGAIN:
+                self._log.warn("socket would have blocked")
+                return None
+            raise
+
---
+++ diyapi_tools/resilient_server.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+resilient_server.py
+
+a class that manages an XREP socket some PUSH clients as a resilient server
+"""
+from collections import namedtuple
+import logging
+
+import zmq
+
+from diyapi_tools.zeromq_util import prepare_ipc_path
+from diyapi_tools.push_client import PUSHClient
+
+# our internal message format
+_message_format = namedtuple("Message", "ident, control body")
+
+class ResilientServer(object):
+    """
+    a class that manages an XREP socket and some PUSH clients 
+    as a resilient server
+    """
+    def __init__(self, context, address, receive_queue):
+        self._log = logging.getLogger("ResilientServer-%s" % (address, ))
+
+        self._context = context
+
+        # we need a valid path for IPC sockets
+        if address.startswith("ipc://"):
+            prepare_ipc_path(address)
+
+        self._xrep_socket = context.socket(zmq.XREP)
+        self._log.debug("binding")
+        self._xrep_socket.bind(address)
+
+        self._receive_queue = receive_queue
+
+        self._dispatch_table = {
+            "resilient-server-handshake" : \
+                self._handle_resilient_server_handshake,
+            "resilient-server-signoff" : \
+                self._handle_resilient_server_signoff,
+        }
+
+        self._active_clients = dict()
+
+    def register(self, pollster):
+        pollster.register_read(self._xrep_socket, self._pollster_callback)
+
+    def unregister(self, pollster):
+        pollster.unregister(self._xrep_socket)
+
+    def close(self):
+        self._xrep_socket.close()
+
+    def send_reply(self, message, data=None):
+        """
+        send a reply to the pull server of the requestor
+        """
+        try:
+            client = self._active_clients[message["client-tag"]]
+        except KeyError:
+            self._log.error("No active client %s message discarded" % (
+                message["client-tag"]
+            ))
+        else:
+            client.send(message, data)
+
+    def _pollster_callback(self, _active_socket, readable, writable):
+        
+        # assume we are readable, because we are only registered for read
+        assert readable
+        message = self._receive_message()      
+
+        # if we get None, that means the socket would have blocked
+        # go back and wait for more
+        if message is None:
+            return
+
+        # we handle our own message traffic, only feed the receive queue
+        # if we don't handle it
+        if message.control["message-type"] in self._dispatch_table:
+            self._dispatch_table[message.control["message-type"]](message)
+        else:
+            self._receive_queue.append((message.control, message.body, ))
+            
+        # send an immediate ack, zeromq sockets seem to always be writable
+        assert writable
+        ack_message = {
+            "message-type" : "resilient-server-ack",
+            "request-id"   : message["request-id"],
+        }
+        self._xrep_socket.send(message.ident, zmq.SNDMORE)
+        self._xrep_socket.send_json(ack_message)
+
+    def _receive_message(self):
+        try:
+            ident = self._xrep_socket.recv(zmq.NOBLOCK)        
+        except zmq.ZMQError, instance:
+            if instance.errno == zmq.EAGAIN:
+                self._log.warn("socket would have blocked")
+                return None
+            raise
+
+        assert self._xrep_socket.rcvmore(), \
+            "Unexpected missing message control part."
+        control = self._xrep_socket.recv_json()
+
+        body = []
+        while self._xrep_socket.rcvmore():
+            body.append(self._xrep_socket.recv())
+
+        # 2011-04-06 dougfort -- if someone is expecting a list and we only get
+        # one segment, they are going to have to deal with it.
+        if len(body) == 0:
+            body = None
+        elif len(body) == 1:
+            body = body[0]
+
+        return _message_format(ident=ident, control=control, body=body)
+
+    def _handle_resilient_server_handshake(self, message):
+        log = logging.getLogger("_handle_resilient_server_handshake")
+        log.info("%(client-tag)s %(client-address)s" % message)
+
+        if message["client-tag"] in self._active_clients:
+            log.warn("replacing existing client %(client-tg)s" % message) 
+            self._active_clients[message["client-tag"]].close()
+
+        self._active_clients[message["client-tag"]] = PUSHClient(
+            self._context,
+            message["client-address"]
+        )
+        
+    def _handle_resilient_server_signoff(self, message):
+        log = logging.getLogger("_handle_resilient_server_signoff")
+        try:
+            client = self._active_clients.pop(message["client-tag"])
+        except KeyError:
+            log.info("no such client: %(client-tag)s" % message)
+        else:
+            log.info("closing client: %(client-tag)s" % message)
+            client.close()
+
+

2ca5859753338dac08b54d05611e162633d6f993
dougfort <dougfort@dougfort.com>
2011-04-26 13:44:14 -0400

    push_client: send directly

--- diyapi_tools/greenlet_push_client.py
+++ diyapi_tools/greenlet_push_client.py
@@ -8,7 +8,6 @@ The purpose is to have multiple clients pushing to a single PULL server
 from collections import namedtuple
 import logging
 
-from gevent.queue import Queue
 from gevent_zeromq import zmq
 
 _message_format = namedtuple("Message", "control body")
@@ -25,37 +24,16 @@ class GreenletPUSHClient(object):
         self._log.debug("connecting to%s" % (address, ))
         self._push_socket.connect(address)
 
-        self._send_queue = Queue(maxsize=None)
-
-    def register(self, pollster):
-        pollster.register_write(
-            self._push_socket, self._pollster_callback
-        )
-
-    def unregister(self, pollster):
-        pollster.unregister(self._push_socket)
-
     def close(self):
         self._push_socket.close()
 
-    def queue_message_for_send(self, message_control, data=None):
-        self._send_queue.put_nowait(
-            _message_format(control=message_control, body=data)
-        )
-
-    def _pollster_callback(self, _active_socket, readable, writable):
-        if writable:
-            while not self._send_queue.empty():
-                message = self._send_queue.get_nowait()
-                self._send_message(message)
-
-    def _send_message(self, message):
+    def send(self, message, data=None):
         self._log.info("sending message: %s" % (
             message.control,  
         ))
-        if message.body is not None:
-            self._push_socket.send_json(message.control, zmq.SNDMORE)
-            self._push_socket.send(message.body, copy=False)
+        if data is not None:
+            self._push_socket.send_json(message, zmq.SNDMORE)
+            self._push_socket.send(data, copy=False)
         else:
-            self._push_socket.send_json(message.control)
+            self._push_socket.send_json(message)
 
--- diyapi_tools/push_client.py
+++ diyapi_tools/push_client.py
@@ -5,12 +5,9 @@ push_client.py
 a class that manages a zeromq PUSH socket as a client,
 The purpose is to have multiple clients pushing to a single PULL server
 """
-from collections import deque, namedtuple
 import logging
 
 import zmq
-
-_message_format = namedtuple("Message", "control body")
 
 class PUSHClient(object):
     """
@@ -24,39 +21,16 @@ class PUSHClient(object):
         self._log.debug("connecting")
         self._push_socket.connect(address)
 
-        self._send_queue = deque()
-
-    def register(self, pollster):
-        pollster.register_write(
-            self._push_socket, self._pollster_callback
-        )
-
-    def unregister(self, pollster):
-        pollster.unregister(self._push_socket)
-
     def close(self):
         self._push_socket.close()
 
-    def queue_message_for_send(self, message_control, data=None):
-        self._send_queue.append(
-            _message_format(control=message_control, body=data)
-        )
-
-    def _pollster_callback(self, _active_socket, readable, writable):
-        while writable:
-            try:
-                message = self._send_queue.popleft()
-            except IndexError:
-                break
-            self._send_message(message)
-
-    def _send_message(self, message):
+    def send(self, message, data=None):
         self._log.info("sending message: %s" % (
-            message.control,  
+            message,  
         ))
-        if message.body is not None:
-            self._push_socket.send_json(message.control, zmq.SNDMORE)
-            self._push_socket.send(message.body, copy=False)
+        if data is not None:
+            self._push_socket.send_json(message, zmq.SNDMORE)
+            self._push_socket.send(data, copy=False)
         else:
-            self._push_socket.send_json(message.control)
+            self._push_socket.send_json(message)
 

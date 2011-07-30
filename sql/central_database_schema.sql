c8b08c0754f6648bc9e8481e43cd1e33e7b867db
dougfort <dougfort@dougfort.com>
2011-07-30 16:24:21 -0400

    revised schema(s) to support S3 emulation

---
+++ sql/central_database_schema.sql
@@ -0,0 +1,51 @@
+/****
+ * schema for centralized diy database
+ ****/
+
+begin;
+
+drop schema if exists diy_central cascade;
+create schema diy_central;
+set search_path to diy_central, public;
+
+/* just a mostly informational table to describe the details of the whole cluster */
+create table "cluster" (
+    id serial primary key,
+    name varchar(255) unique not null,
+    node_count int4 not null default 10,
+    replication_level int4 not null default 3,
+    creation_time timestamp
+);
+
+create table node (
+    id serial primary key,
+    cluster_id int4 not null,
+    node_number_in_cluster int4 not null,
+    name varchar(255) unique not null,
+    hostname varchar(255) not null,
+    offline bool not null default false,
+    creation_time timestamp
+);
+
+create sequence collection_id_seq;
+create table collection (
+    id int4 not null default nextval('diy_central.collection_id_seq'),
+    name varchar(1024) unique not null,
+    avatar_id int4 not null,
+    timestamp timestamp not null
+);
+
+/* get avatar_id for collection name */
+create index collection_name_idx on collection("name");
+
+/* get all collection names for avatar_id */
+create index collection_avatar_id_name_idx on collection("avatar_id", "name");
+
+
+grant all privileges on schema diy_central to pandora;
+grant all privileges on all tables in schema diy_central to pandora;
+grant all privileges on all sequences in schema diy_central to pandora;
+
+rollback;
+/*commit;*/
+

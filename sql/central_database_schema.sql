ee42b3f303e882b233d99ee2b8f0303d9e1a2a03
dougfort <dougfort@dougfort.com>
2011-07-31 14:05:48 -0400

    central_database: add cluster_id to the collection table, add the space_accounting table

--- sql/central_database_schema.sql
+++ sql/central_database_schema.sql
@@ -29,7 +29,8 @@ create table node (
 
 create sequence collection_id_seq;
 create table collection (
-    id int4 not null default nextval('diy_central.collection_id_seq'),
+    id int4 unique not null default nextval('diy_central.collection_id_seq'),
+    cluster_id int4 not null references diy_central.cluster(id),
     name varchar(1024) unique not null,
     avatar_id int4 not null,
     timestamp timestamp not null
@@ -41,6 +42,13 @@ create index collection_name_idx on collection("name");
 /* get all collection names for avatar_id */
 create index collection_avatar_id_name_idx on collection("avatar_id", "name");
 
+create table space_accounting(
+   collection_id int4 not null references diy_central.collection(id),
+   timestamp timestamp not null,
+   bytes_added int8 not null default 0,
+   bytes_removed int8 not null default 0,
+   bytes_retrieved int8 not null default 0
+);
 
 grant all privileges on schema diy_central to pandora;
 grant all privileges on all tables in schema diy_central to pandora;

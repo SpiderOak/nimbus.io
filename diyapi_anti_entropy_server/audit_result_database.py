c7f9b6a96452f346972f3864c5bfd30dbc7aba8c
dougfort <dougfort@dougfort.com>
2011-08-10 13:43:14 -0400

    anit_entropy_server: fize queries for postgres

--- diyapi_anti_entropy_server/audit_result_database.py
+++ diyapi_anti_entropy_server/audit_result_database.py
@@ -5,7 +5,6 @@ audit_result_database.py
 wrap access to the diy_central.audit_result_table 
 """
 import logging
-import os
 
 state_audit_started = "audit-started"
 state_audit_waiting_retry = "audit-waiting-retry"
@@ -16,31 +15,31 @@ state_audit_error = "audit-error"
 _start_audit_command = """
 INSERT INTO diy_central.audit_result
 (avatar_id, state, audit_started)
-VALUES(%s, '%s', '%s'::timestamp);
+VALUES(%s, %s, %s::timestamp);
 """.strip()
 
 _audit_retry_command = """
 UPDATE diy_central.audit_result
-SET state = '%s', audit_started = NULL
+SET state = %s, audit_started = NULL
 WHERE diy_central.audit_result_id = %s;
 """.strip()
 
 _restart_audit_command = """
 UPDATE diy_central.audit_result
-SET state = '%s', audit_started = '%s'::timestamp
+SET state = %s, audit_started = %s::timestamp
 WHERE diy_central.audit_result_id = %s;
 """.strip()
 
 _audit_result_command = """
 UPDATE diy_central.audit_result
-SET state = '%s', audit_finished = '%s'::timestamp
+SET state = %s, audit_finished = %s::timestamp
 WHERE diy_central.audit_result_id = %s;
 """.strip()
 
 _ineligible_query = """
 SELECT avatar_id FROM diy_central.audit_result
-WHERE COALESCE(state, 'audit-successful') != 'audit-successful'
-OR COALESCE(audit_finished, now()) > '%s'::timestamp;
+WHERE COALESCE(state, audit-successful) != audit-successful
+OR COALESCE(audit_finished, now()) > %s::timestamp;
 """.strip()
 
 _clear_command = """
@@ -72,53 +71,53 @@ class AuditResultDatabase(object):
 
     def wait_for_retry(self, row_id):
         """update a row to mark waiting for retry"""
-        command = _audit_retry_command % (
-            state_audit_waiting_retry, row_id, 
+        self._connection.execute(
+            _audit_retry_command,
+            [state_audit_waiting_retry, row_id, ]
         )
-        self._connection.execute(command)
         self._connection.commit()
 
     def too_many_retries(self, row_id):
         """update a row to mark too many retries"""
-        command = _audit_retry_command % (
-            state_audit_too_many_retries, row_id, 
+        self._connection.execute(
+            _audit_retry_command,
+            [state_audit_too_many_retries, row_id, ]
         )
-        self._connection.execute(command)
         self._connection.commit()
 
     def restart_audit(self, row_id, timestamp):
         """update a row to mark waiting for retry"""
-        command = _restart_audit_command % (
-            state_audit_started, timestamp, row_id,  
+        self._connection.execute(
+            _restart_audit_command,
+            [state_audit_started, timestamp, row_id, ]
         )
-        self._connection.execute(command)
         self._connection.commit()
 
     def successful_audit(self, row_id, timestamp):
         """update a row to mark success of an audit"""
-        command = _audit_result_command % (
-            state_audit_successful, timestamp, row_id, 
+        self._connection.execute(
+            _audit_result_command,
+            [state_audit_successful, timestamp, row_id, ]
         )
-        self._connection.execute(command)
         self._connection.commit()
 
     def audit_error(self, row_id, timestamp):
         """update a row to mark failure of an audit"""
-        command = _audit_result_command % (
-            state_audit_error, timestamp, row_id, 
+        self._connection.execute(
+            _audit_result_command,
+            [state_audit_error, timestamp, row_id, ]
         )
-        self._connection.execute(command)
         self._connection.commit()
 
     def ineligible_avatar_ids(self, cutoff_timestamp):
         """return a list of avatar_ids that are NOT eligible for audit"""
-        command = _ineligible_query % (cutoff_timestamp, )
-        result = self._connection.fetch_all_rows(command)
+        result = self._connection.fetch_all_rows(
+            _ineligible_query, [cutoff_timestamp, ]
+        )
         return [avatar_id for (avatar_id, ) in result]
 
     def _clear_avatar(self, avatar_id):
         """for testing: remove all rows for an avatar"""
-        command = _clear_command % (avatar_id, )
-        self._connection.execute(command)
+        self._connection.execute(_clear_command, [avatar_id, ])
         self._connection.commit()
 

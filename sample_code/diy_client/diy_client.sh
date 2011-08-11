626b9da3df3a197bd5b4e31409f728b75674f3c4
dougfort <dougfort@dougfort.com>
2011-08-11 10:07:13 -0400

    moved sample_code to the lumberyard package as examples

--- sample_code/diy_client/diy_client.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# script fpr running the client on Linux
-export DIYAPI="${HOME}/spideroak_diyapi"
-export PYTHONPATH="${DIYAPI}"
-
-python "${DIYAPI}/sample_code/diy_client/diy_client_main.py"

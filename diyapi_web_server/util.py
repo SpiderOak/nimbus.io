f29c54c98fa175a2d43027db6071e4925073d305
dougfort <dougfort@dougfort.com>
2011-08-28 13:28:18 -0400

    web_server: some security fixes from Alan

--- diyapi_web_server/util.py
+++ diyapi_web_server/util.py
@@ -4,6 +4,7 @@ util.py
 
 Utility functions for web server
 """
+from itertools import izip
 import re
 
 
@@ -14,3 +15,15 @@ def get_username_from_req(req):
     m = _HOST_USERNAME_RE.match(req.host)
     if m:
         return m.group(1).lower()
+
+def sec_str_eq(str1, str2):
+    "efficient constant time string comparison for arbirtrary strings"
+    if not len(str1) == len(str2):
+        return False
+    
+    match_count = 0
+    for a, b in izip(str1, str2):
+        match_count += 1 if a == b else 0
+            
+    return match_count == len(str1)
+

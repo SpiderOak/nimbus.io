cadab264b3032e3798267c045f7c125b0e4d2265
dougfort <dougfort@dougfort.com>
2011-09-09 11:19:40 -0400

    start documenting resilient client

--- tools/resilient_client.py
+++ tools/resilient_client.py
@@ -31,7 +31,37 @@ _status_name = {
 
 class ResilientClient(object):
     """
-    a class that manages a zeromq XREQ socket as a client
+    ResilientClient uses two zeromq patterns to maintain a connection
+    to a resilient server.
+
+    - request-reply_ for sending messages and for receiving acknowledgements
+    - pipeline_ for receiving the actual replies to messages
+
+    Each process maintains a single PULL_ socket for all of its resilient 
+    clients. The address of this socket is the member **_client_address**.
+    The client includes this in every message along with **_client_tag** which
+    uniquely identifies the client to the server
+
+    Each resilient client maintains its own DEALER_ socket **_dealer_socket**.
+
+    At startup the client sends a *handshake* message to the server. The client
+    is not considered connected until it gets an ack fro the handshake.
+
+    Normal workflow:
+    
+    1. The client pops a message from **_send_queue**
+    2. The client sends the message over the DEALER_ socket
+    3. The client waits for and ack from the server. It does not send any
+       other messages until it gets the ack.
+    4. When the ack is received the client repeats from step 1.
+    5. The actual reply from the server comes to the PULL_ socket and is
+       handled outside the client
+
+    .. _request-reply: http://api.zeromq.org/2-1:zmq-socket#toc3
+    .. _pipeline: http://api.zeromq.org/2-1:zmq-socket#toc11
+    .. _PULL: http://api.zeromq.org/2-1:zmq-socket#toc13
+    .. _DEALER: http://api.zeromq.org/2-1:zmq-socket#toc6
+
     """
 
     def __init__(

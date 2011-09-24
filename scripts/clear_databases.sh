9c55a005280f54ebdd2334dcbfde6c57176f7828
dougfort <dougfort@dougfort.com>
2011-09-24 12:10:10 -0400

    fix scripts that clear the database

--- scripts/clear_databases.sh
+++ scripts/clear_databases.sh
@@ -3,8 +3,8 @@
 export NIMBUSIO="${HOME}/nimbus.io"
 for i in 01 02 03 04 05 06 07 08 09 10
 do
-   psql -d "nimbusio_node.multi-node-$i" -f "${NIMBUSIO}/sql/clear_local_database.sql"
+   psql -d "nimbusio_node.multi-node-$i" -f "${NIMBUSIO}/sql/clear_nimbusio_node.sql"
 done
-psql -d "nimbusio_central." -f "${NIMBUSIO}/sql/clear_central_database.sql"
-psql -d "nimbusio_central." -f "${NIMBUSIO}/sql/populate_test_cluster.sql"
+psql -d "nimbusio_central" -f "${NIMBUSIO}/sql/clear_nimbusio_central.sql"
+psql -d "nimbusio_central" -f "${NIMBUSIO}/sql/populate_nimbusio_central_test_cluster.sql"
 
--- sql/clear_nimbusio_node.sql
+++ sql/clear_nimbusio_node.sql
@@ -2,4 +2,4 @@ delete from nimbusio_node.segment;
 delete from nimbusio_node.segment_sequence;
 delete from nimbusio_node.value_file;
 delete from nimbusio_node.meta;
-delete from nimbus_io.conjoined;
+delete from nimbusio_node.conjoined;

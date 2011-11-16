ff4e7cfb059e19c3f41967bf6b06c7ee581e83df
Alan Fairless <alan@spideroak.com>
2011-11-15 18:02:06 -0600

    wip

--- test/nimbusio_sim/cluster_config.py
+++ test/nimbusio_sim/cluster_config.py
@@ -1,6 +1,8 @@
 """
 """
 
+import os
+
 class ClusterConfig(object):
     """
     Hold configuration state for cluster
@@ -8,11 +10,17 @@ class ClusterConfig(object):
     def __init__(self, args):
         self.config = args
         self.base_ports = dict(
-            writer =            args.baseport,
-            reader =            1 * args.nodecount + args.baseport,
-            anti_entropy =      2 * args.nodecount + args.baseport, 
-            handoff =           3 * args.nodecount + args.baseport, 
-            handoff_pipeline =  4 * args.nodecount + args.baseport )
+            writer =                    args.baseport,
+            reader =                    1 * args.nodecount + args.baseport,
+            anti_entropy =              2 * args.nodecount + args.baseport, 
+            handoff =                   3 * args.nodecount + args.baseport, 
+            handoff_pipeline =          4 * args.nodecount + args.baseport,
+            event_publisher_pub  =      5 * args.nodecount + args.baseport,
+            event_aggregator_pull =     6 * args.nodecount + args.baseport,
+            # these two only need single ports
+            space_accounting =          1 + 7 * args.nodecount + args.baseport,
+            space_accounting_pipeline = 2 + 7 * args.nodecount + args.baseport, 
+        )
 
     def __getattr__(self, name):
         if not hasattr(self.config, name):
@@ -20,13 +28,22 @@ class ClusterConfig(object):
         return getattr(self.config, name)
 
     @property
+    def log_path(self):
+        return os.path.join(self.basedir, "logs")
+
+    def _node_service_addresses(self, name):
+        return [ "tcp://127.0.0.1:%s" % i
+            for i in range(self.base_ports[name],
+                           self.base_ports[name] + self.nodecount)]
+
+    @property
+    def port_range(self):
+        "(lowest, highest, ) local ports used"
+        return (min(self.base_ports.values()), max(self.base_ports.values()), )
+
+    @property
     def node_names(self):
         return [self.nodenamepattern % i for i in range(1, self.nodecount + 1)]
-
-    def _node_service_addresses(name):
-        return [ "tcp://127.0.0.1:%s" % i
-            for i in range(self.base_ports['name']
-                           self.base_ports['name'] + self.nodecount)]
 
     @property
     def space_accounting_server_address(self):
@@ -37,6 +54,19 @@ class ClusterConfig(object):
     def space_accounting_pipeline_address(self):
         return "tcp://127.0.0.1:%d" % ( max(self.base_ports.values()) 
                                         + self.nodecount + 1 )
+
+    @property
+    def event_publisher_pull_addresses(self):
+        return [ "ipc:///tmp/nimbusio-event-publisher-%s/socket" % n
+                 for n in self.node_names ]
+
+    @property
+    def event_publisher_pub_addresses(self):
+        return self._node_service_addresses('event_publisher_pub')
+
+    @property
+    def event_aggregator_pub_addresses(self):
+        return self._node_service_addresses('event_aggregator_pull')
 
     @property
     def data_writer_addresses(self):
--- test/nimbusio_sim/nimbusio_sim_main.py
+++ test/nimbusio_sim/nimbusio_sim_main.py
@@ -10,25 +10,50 @@ import sys
 
 from test.nimbusio_sim.command_interpreter import CommandInterpreter
 from test.nimbusio_sim.options import parse_cmdline
+from test.nimbusio_sim.cluster_config import ClusterConfig
 
-_log_path = u"%s/nimbusio_sim.log" % (os.environ["NIMBUSIO_LOG_DIR"], )
+_log_name = u"nimbusio_sim.log"
 _log_format_template = u'%(asctime)s %(levelname)-8s %(name)-20s: %(message)s'
 
-def _initialize_logging():
+def _initialize_logging(config):
     """initialize the log"""
     log_level = logging.DEBUG
-    handler = logging.FileHandler(_log_path, mode="a", encoding="utf-8" )
+    handler = logging.FileHandler(
+        os.path.join(config.log_path, _log_name), mode="a", encoding="utf-8" )
     formatter = logging.Formatter(_log_format_template)
     handler.setFormatter(formatter)
 
     logging.root.addHandler(handler)
     logging.root.setLevel(log_level)
 
+def sanity_check(config):
+    # TODO
+    # if args.create, make sure basedir is empty or does not exist
+    # if not args.create, make sure basedir has a config file in it
+    # make sure all ports in range are bindable
+    return True
+
+def ensure_paths(config):
+    "ensure that all the directories needed exist"
+    # TODO
+    pass
+
 def main():
     """Main entry point for cluster simulator"""
+
     args = parse_cmdline()
+    config = ClusterConfig(args)
+    print repr(args)
+
+    if not sanity_check(config):
+        return 1
+
+    ensure_paths(config)
+
+    #import pdb
+    #pdb.set_trace()
+    _initialize_logging(config)
     return
-    _initialize_logging()
     log = logging.getLogger("main")
     log.info("progam starts")
 

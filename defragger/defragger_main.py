bff85711c2991edf3a2c39b25cb0a8a700d1fcea
dougfort <dougfort@spideroak.com>
2012-01-05 16:04:06 -0500

    Ticket #1656 defragger: added zeromq for event push client

--- defragger/defragger_main.py
+++ defragger/defragger_main.py
@@ -8,8 +8,11 @@ import signal
 import sys
 from threading import Event
 
+import zmq
+
 from tools.standard_logging import initialize_logging
 from tools.database_connection import get_node_local_connection
+from tools.event_push_client import EventPushClient, unhandled_exception_topic
 
 _local_node_name = os.environ["NIMBUSIO_NODE_NAME"]
 _log_path = "{0}/nimbusio_defragger_{1}.log".format(
@@ -23,7 +26,7 @@ def _create_signal_handler(halt_event):
         halt_event.set()
     return cb_handler
 
-def _defrag_pass(local_connection):
+def _defrag_pass(local_connection, event_push_client):
     """
     Make a single defrag pass
     return the number of bytes defragged
@@ -44,6 +47,11 @@ def main():
     halt_event = Event()
     signal.signal(signal.SIGTERM, _create_signal_handler(halt_event))
 
+    zmq_context =  zmq.Context()
+
+    event_push_client = EventPushClient(zmq_context, "defragger")
+    event_push_client.info("program-start", "defragger starts")  
+
     local_connection = None
 
     while not halt_event.is_set():
@@ -53,6 +61,12 @@ def main():
             try:
                 local_connection = get_node_local_connection()
             except Exception as instance:
+                exctype, value = sys.exc_info()[:2]
+                event_push_client.exception(
+                    "database exception",
+                    str(value),
+                    exctype=exctype.__name__
+                )
                 log.exception("Exception connecting to database")
                 halt_event.wait(_database_retry_interval)
                 continue
@@ -60,11 +74,18 @@ def main():
         # try one defrag pass
         bytes_defragged = 0
         try:
-            bytes_defragged = _defrag_pass(local_connection)
+            bytes_defragged = _defrag_pass(
+                local_connection, event_push_client
+            )
         except KeyboardInterrupt:
             halt_event.set()
         except Exception as instance:
             log.exception(str(instance))
+            event_push_client.exception(
+                unhandled_exceptiopn_topic,
+                str(value),
+                exctype=exctype.__name__
+            )
 
         log.info("bytes defragged = {0}".format(bytes_defragged))
 
@@ -84,6 +105,9 @@ def main():
                 
     if local_connection is not None:
         local_connection.close()
+
+    event_push_client.close()
+    zmq_context.term()
 
     log.info("program terminates normally")
     return 0

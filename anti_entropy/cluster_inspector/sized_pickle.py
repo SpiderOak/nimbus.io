619a5b89a4aea97e5b45aff8bfa75fa4ef0b57ec
dougfort <dougfort@spideroak.com>
2012-04-08 20:47:04 -0400

    anti_entropy: sized pickle

---
+++ anti_entropy/cluster_inspector/sized_pickle.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+sized_pickle.py
+
+store and retrieve data as a sized_pickle: a pickle with the first 4 bytes 
+representing an integer size
+"""
+
+import pickle
+import struct
+
+_pickle_length_format_ = "!I"
+_pickle_length_size = struct.calcsize(_pick_length_format)
+
+def store_sized_pickle(data, file_object):
+    """
+    store a sized pickle in the file object
+    """
+    pickled_data = pickle.dumps(data)
+    file_object.write(pickled_data)
+    file_object.write(struct.pack, _pickle_length_format, len(pickled_data))
+
+def retrieve_sized_pickle(file_object):
+    length_str = file_object.read(_pickle_length_size)
+    pickle_length = struct.unpack(_pickle_length_format, length_str)
+    pickled_data = file_object.read(pickle_length)
+    return pickle.loads(pickled_data)
+

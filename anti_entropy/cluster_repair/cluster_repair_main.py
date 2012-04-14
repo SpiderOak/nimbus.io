d5b5398ce19c5a9ca5ddd133d035c852f1f73b5a
dougfort <dougfort@spideroak.com>
2012-04-14 17:44:18 -0400

    cluster_repair: initial; shell

---
+++ anti_entropy/cluster_repair/cluster_repair_main.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+cluster_repair_main.py
+
+repair defective node data
+"""
+import logging
+import os
+import os.path
+import signal
+import sys
+from threading import Event
+
+import zmq
+
+from tools.standard_logging import initialize_logging
+from tools.event_push_client import EventPushClient, unhandled_exception_topic
+
+class ClusterRepairError(Exception):
+    pass
+
+_local_node_name = os.environ["NIMBUSIO_NODE_NAME"]
+_log_path = "{0}/nimbusio_cluster_repair_{1}.log".format(
+    os.environ["NIMBUSIO_LOG_DIR"], _local_node_name)
+
+def _create_signal_handler(halt_event):
+    def cb_handler(*_):
+        halt_event.set()
+    return cb_handler
+
+def main():
+    """
+    main entry point
+
+    return 0 for success (exit code)
+    """
+    initialize_logging(_log_path)
+    log = logging.getLogger("main")
+    log.info("program starts")
+
+    halt_event = Event()
+    signal.signal(signal.SIGTERM, _create_signal_handler(halt_event))
+
+    zmq_context =  zmq.Context()
+
+    event_push_client = EventPushClient(zmq_context, "cluster_repair")
+    event_push_client.info("program-start", "cluster_repair starts")  
+
+
+    try:
+        pass
+
+    except KeyboardInterrupt:
+        halt_event.set()
+        connection.rollback()
+    except Exception as instance:
+        log.exception(str(instance))
+        event_push_client.exception(
+            unhandled_exception_topic,
+            str(instance),
+            exctype=instance.__class__.__name__
+        )
+        return -3
+
+    event_push_client.close()
+    zmq_context.term()
+
+    log.info("program terminates normally")
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+
---
+++ scripts/run_cluster_repair_on_sim_cluster.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# simple script to run the node inspector against a running cluster sim.
+# pass basedir of cluster sim as $1
+# finds config for cluster under basedi
+
+set -x
+set -e
+
+BASEDIR=$1
+
+if [ ! -d $BASEDIR ]; then
+    echo "basedir '$BASEDIR' does not exist"
+    exit 1
+fi
+
+PYTHON="${HOME}/python_env/nimbus.io.0.0.1/bin/python3"
+
+# pull in environment settings from the simulated cluster 
+pushd "${BASEDIR}/config"
+source node_01_config.sh
+popd
+
+export NIMBUSIO_LOG_LEVEL="DEBUG"
+
+# run unit tests with identity file
+$PYTHON "${HOME}/nimbus.io/anti_entropy/cluster_repair/cluster_repair_main.py"
+

857f76c1fd4bb3f4ddc763fea8bf336694c63963
dougfort <dougfort@spideroak.com>
2012-06-01 15:18:04 -0400

    Change resilient server (and resilient clients) to use req/rep sockets

--- tools/resilient_server.py
+++ tools/resilient_server.py
@@ -2,7 +2,7 @@
 """
 resilient_server.py
 
-a class that manages a ROUTER (aka XREP) socket and some PUSH clients 
+a class that manages a REP socket and some PUSH clients 
 as a resilient server
 """
 import logging
@@ -15,19 +15,19 @@ from tools.data_definitions import message_format
 
 class ResilientServer(object):
     """
-    a class that manages a ROUTER (aka XREP) socket and some PUSH clients 
+    a class that manages a REP socket and some PUSH clients 
     as a resilient serve.
 
     The resilient server receives messages from resilient clients over a
-    ROUTER socket and sends replies using PUSH clients.
+    REP socket and sends replies using PUSH clients.
 
     """
     def __init__(self, context, address, receive_queue):
         self._log = logging.getLogger("ResilientServer-%s" % (address, ))
 
         self._context = context
-        self._router_socket = context.socket(zmq.XREP)
-        self._router_socket.setsockopt(zmq.LINGER, 1000)
+        self._rep_socket = context.socket(zmq.REP)
+        self._rep_socket.setsockopt(zmq.LINGER, 1000)
 
         # a server can bind to multiple zeromq addresses
         if type(address) in [list, tuple, ]:
@@ -41,7 +41,7 @@ class ResilientServer(object):
                 prepare_ipc_path(bind_address)
 
             self._log.debug("binding to %s" % (bind_address, ))
-            self._router_socket.bind(bind_address)
+            self._rep_socket.bind(bind_address)
 
         self._receive_queue = receive_queue
 
@@ -58,19 +58,19 @@ class ResilientServer(object):
         """
         resiter ourselves with the pollster for reads
         """
-        pollster.register_read(self._router_socket, self.pollster_callback)
+        pollster.register_read(self._rep_socket, self.pollster_callback)
 
     def unregister(self, pollster):
         """
         unregister from the polster
         """
-        pollster.unregister(self._router_socket)
+        pollster.unregister(self._rep_socket)
 
     def close(self):
         """
         close out ROUTER socket and and all the PUSH clients we are holding
         """
-        self._router_socket.close()
+        self._rep_socket.close()
         for client in self._active_clients.values():
             client.close()
 
@@ -106,70 +106,43 @@ class ResilientServer(object):
         assert readable
         message = self._receive_message()      
 
-        # if we get None, that means the socket would have blocked
-        # go back and wait for more
-        if message is None:
-            return
+        ack_message = {
+            "message-type" : "resilient-server-ack",
+            "message-id"   : message.control["message-id"],
+            "incoming-type": message.control["message-type"],
+            "accepted"     : None
+        }
 
-        # we handle our own message traffic (i.e. resilient client handshakes
-        # and signoffs).
-        # otherwise, feed message into the receive queue to be handled
-        # elsewhere
         if message.control["message-type"] in self._dispatch_table:
             self._dispatch_table[message.control["message-type"]](
                 message.control, message.body
             )
-            self._send_ack(
-                message.control["message-type"],
-                message.ident, 
-                message.control["message-id"])
+            ack_message["accepted"] = True
         elif not "client-tag" in message.control:
             self._log.error("receive: invalid message '%s'" % (
                 message.control, 
             ))
+            ack_message["accepted"] = False
         else:
             if message.control["client-tag"] in self._active_clients:
                 self._receive_queue.append((message.control, message.body, ))
-                self._send_ack(
-                    message.control["message-type"],
-                    message.ident, 
-                    message.control["message-id"]
-                )
+                ack_message["accepted"] = True
             else:
                 self._log.error(
                     "receive: No active client %s message discarded" % (
                         message.control["client-tag"]
                     )
                 )
+                ack_message["accepted"] = False
 
-    def _send_ack(self, incoming_type, message_ident, message_id):            
-        # send an immediate ack, zeromq sockets seem to always be writable
-        self._log.debug("sending ack: %s %s" % (incoming_type, message_id, ))
-        ack_message = {
-            "message-type" : "resilient-server-ack",
-            "message-id"   : message_id,
-            "incoming-type": incoming_type,
-        }
-        self._router_socket.send(message_ident, zmq.SNDMORE)
-        self._router_socket.send_json(ack_message)
+        self._rep_socket.send_json(ack_message)
 
     def _receive_message(self):
-        try:
-            ident = self._router_socket.recv(zmq.NOBLOCK)        
-        except zmq.ZMQError, instance:
-            if instance.errno == zmq.EAGAIN:
-                self._log.warn("socket would have blocked")
-                return None
-            raise
-
-        #self._log.debug("hrm %r" % ( self._router_socket, ))
-        assert self._router_socket.rcvmore, \
-            "Unexpected missing message control part."
-        control = self._router_socket.recv_json()
+        control = self._rep_socket.recv_json()
 
         body = []
-        while self._router_socket.rcvmore:
-            body.append(self._router_socket.recv())
+        while self._rep_socket.rcvmore:
+            body.append(self._rep_socket.recv())
 
         # 2011-04-06 dougfort -- if someone is expecting a list and we only get
         # one segment, they are going to have to deal with it.
@@ -178,7 +151,7 @@ class ResilientServer(object):
         elif len(body) == 1:
             body = body[0]
 
-        return message_format(ident=ident, control=control, body=body)
+        return message_format(ident=None, control=control, body=body)
 
     def _handle_resilient_server_handshake(self, message, _data):
         log = logging.getLogger("_handle_resilient_server_handshake")

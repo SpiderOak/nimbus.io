3dc39afe996410bce680151b5042c5c00a9d2fb8
alan <alan@spideroak.com>
2012-06-10 13:19:24 -0500

    working common runsv script

--- scripts/runsv_service_run.sh
+++ scripts/runsv_service_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#set -e
+#set -x
+
 export NIMBUS_IO_RUN_SERVICE_DIR="$(cd $(dirname $0); pwd)"
 
 export NIMBUS_IO_RUN_SERVICE_NAME="$(basename $NIMBUS_IO_RUN_SERVICE_DIR)"
@@ -7,7 +10,7 @@ export NIMBUS_IO_RUN_SERVICE_NAME="$(basename $NIMBUS_IO_RUN_SERVICE_DIR)"
 # some sites install the service directory for each service NAME as
 # /etc/service/nimbus.io.NAME, so remove "nimbus.io" from the left to find the
 # name of the program to run.
-export NIMBUS_IO_RUN_PROGRAM_NAME="${NIMBUS_IO_RUN_SERVICE_NAME#nimbus.io}"
+export NIMBUS_IO_RUN_PROGRAM_NAME="${NIMBUS_IO_RUN_SERVICE_NAME#nimbus.io.}"
 
 NODE_CONFIG="/etc/default/nimbus.io"
 
@@ -39,14 +42,21 @@ prog_dir="$NIMBUS_IO_SRC_PATH/$NIMBUS_IO_RUN_PROGRAM_NAME"
 prog_script="${NIMBUS_IO_RUN_PROGRAM_NAME}_run.sh"
 prog_file="${NIMBUS_IO_RUN_PROGRAM_NAME}_main.py"
 
-# if there's a bash run script, just exec that.
-if [[ -e "$prog_script" ]]; then
-    exec chpst -u ${NIMBUS_IO_USERNAME:?}:${NIMBUS_IO_GROUPNAME:?} $prog_script
-fi
+# if there's a bash run script, we'll just exec that
+if [[ -e "$prog_dir/$prog_script" ]]; then
+    prog_cmd="$prog_dir/$prog_script"
 
-py_interpreter="$PYTHON2"
-head -n 1 "$py_script" | grep -q python3 && py_interpreter="$PYTHON3"
+# otherwise look for a python main file
+elif [[ -e "$prog_dir/$prog_file" ]]; then
+    prog_cmd="$prog_dir/$prog_file"
+    py_interpreter="$PYTHON2"
+    head -n 1 "$prog_cmd" | grep -q python3 && py_interpreter="$PYTHON3"
+    prog_cmd="$py_interpreter $prog_cmd"
+else
+    echo "Cannot find executable"
+    exit 1
+fi
 
 exec chpst \
     -u ${NIMBUS_IO_USERNAME:?}:${NIMBUS_IO_GROUPNAME:?} \
-    $py_interpreter $prog_file
+    $prog_cmd

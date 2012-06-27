bb1093aa5e74b6a4fe5ec92e98f72734e5d10c37
alan <alan@spideroak.com>
2012-06-26 21:40:13 -0500

    fix web director runsv run script

--- web_director/web_director_run.sh
+++ web_director/web_director_run.sh
@@ -2,5 +2,5 @@
 
 PROG_DIR="$(cd $(dirname $0) ; pwd)"
 
-tproxy -b ${NIMBUS_IO_SERVICE_DOMAIN:?}:${NIMBUSIO_WEB_SERVER_PORT:?} \
-    $PROG_DIR/web_director_main.py
+exec tproxy -b ${NIMBUS_IO_SERVICE_DOMAIN:?}:${NIMBUSIO_WEB_SERVER_PORT:?} \
+    $PROG_DIR/web_director_main.py 2>&1

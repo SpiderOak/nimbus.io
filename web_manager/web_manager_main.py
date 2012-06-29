8553cdc14b7a47540b0fa773c2211edc47db006e
dougfort <dougfort@spideroak.com>
2012-06-29 10:04:22 -0400

    web_manager: use NIMBUSIO_MANAGEMENT_API_REQUEST_DEST

--- web_manager/web_manager_main.py
+++ web_manager/web_manager_main.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 _local_node_name = os.environ["NIMBUSIO_NODE_NAME"]
 _log_path = "{0}/nimbusio_web_manager_{1}.log".format(
     os.environ["NIMBUSIO_LOG_DIR"], _local_node_name)
+_management_api_dest = os.environ["NIMBUSIO_MANAGEMENT_API_REQUEST_DEST"]
 
 @app.route("/")
 def hello():
@@ -24,8 +25,9 @@ if __name__ == "__main__":
     initialize_logging(_log_path)
     log = logging.getLogger("__main__")
     log.info("program starts")
+    host, port_str = _management_api_dest.split(":")
     try:
-        app.run()
+        app.run(host=host, port=int(port_str))
     except Exception:
         log.exception("app.run")
     log.info("program terminates")

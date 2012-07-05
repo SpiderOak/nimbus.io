cc02861c7b72ed882ff6fda5e0a3e8df816a291d
dougfort <dougfort@spideroak.com>
2012-07-05 14:49:03 -0400

    web_manager: use exec to run

--- web_manager/web_manager_run.sh
+++ web_manager/web_manager_run.sh
@@ -3,6 +3,6 @@
 PROG_DIR="$(cd $(dirname $0) ; pwd)"
 PYTHON="python2.7"
 
-${PYTHON} "${PROG_DIR}/web_manager_main.py" \ 
+exec ${PYTHON} "${PROG_DIR}/web_manager_main.py" \ 
    2>&1 > /var/log/pandora/nimbus/web_manager.log
 

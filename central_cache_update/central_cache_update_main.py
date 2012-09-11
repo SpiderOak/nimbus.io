131bc2c3f46a739dfa554261c8922c1ca4aada93
dougfort <dougfort@spideroak.com>
2012-09-11 11:26:14 -0400

    Ticket #47 Update/Invalidate memcache records when central DB changes Completed basic implementation, simply deleting memcache keys

--- central_cache_update/central_cache_update_main.py
+++ central_cache_update/central_cache_update_main.py
@@ -20,11 +20,11 @@ from tools.standard_logging import initialize_logging
 from tools.zeromq_util import is_interrupted_system_call, \
         prepare_ipc_path
 from tools.process_util import set_signal_handler
+from tools.data_definitions import memcached_central_key_template
 
 _log_path = "{0}/nimbusio_central_cache_update.log".format(
     os.environ["NIMBUSIO_LOG_DIR"]) 
 _skeeter_pub_socket_uri = os.environ["NIMBUSIO_CENTRAL_SKEETER_URI"]
-_heartbeat_channel = "heartbeat"
 _cache_update_channel = "nimbusio_central_cache_update"
 _memcached_host = os.environ.get("NIMBUSIO_MEMCACHED_HOST", "localhost")
 _memcached_port = int(os.environ.get("NIMBUSIO_MEMCACHED_PORT", "11211"))
@@ -37,12 +37,59 @@ def _create_sub_socket(zeromq_context):
     sub_socket = zeromq_context.socket(zmq.SUB)
     log.info("connecting to {0}".format(_skeeter_pub_socket_uri))
     sub_socket.connect(_skeeter_pub_socket_uri)
-    log.info("subscribing to {0}".format(_heartbeat_channel))
-    sub_socket.setsockopt(zmq.SUBSCRIBE, _heartbeat_channel)
     log.info("subscribing to {0}".format(_cache_update_channel))
     sub_socket.setsockopt(zmq.SUBSCRIBE, _cache_update_channel)
 
     return sub_socket
+
+def _process_collection_event(memcached_client, event_data):
+    log = logging.getLogger("_process_collection_event")
+    if event_data["event"] != "UPDATE":
+        log.error("unknown event {0}".format(event_data))
+        return
+
+    lookup_field = "id"
+    key = memcached_central_key_template.format(
+        "collection", 
+        lookup_field, 
+        event_data["old"][lookup_field])
+    result = memcached_client.delete(key)
+    log.info("delete {0} result = {1}".format(key, result))
+
+def _process_customer_event(memcached_client, event_data):
+    log = logging.getLogger("_process_customer_event")
+    if event_data["event"] != "UPDATE":
+        log.error("unknown event {0}".format(event_data))
+        return
+
+    lookup_fields = ["id", "username", ]
+    for lookup_field in lookup_fields:
+        key = memcached_central_key_template.format(
+            "customer", 
+            lookup_field, 
+            event_data["old"][lookup_field])
+        result = memcached_client.delete(key)
+        log.info("delete {0} result = {1}".format(key, result))
+
+def _process_customer_key_event(memcached_client, event_data):
+    log = logging.getLogger("_process_customer_key_event")
+    if event_data["event"] != "UPDATE":
+        log.error("unknown event {0}".format(event_data))
+        return
+
+    lookup_field = "id"
+    key = memcached_central_key_template.format(
+        "customer_key", 
+        lookup_field, 
+        event_data["old"][lookup_field])
+    result = memcached_client.delete(key)
+    log.info("delete {0} result = {1}".format(key, result))
+
+_dispatch_table = {
+    "collection"   : _process_collection_event,
+    "customer"     : _process_customer_event,
+    "customer_key" : _process_customer_key_event,
+}
 
 def _process_one_event(memcached_client, expected_sequence, topic, meta, data):
     log = logging.getLogger("event")
@@ -56,34 +103,6 @@ def _process_one_event(memcached_client, expected_sequence, topic, meta, data):
     meta_dict["timestamp"] = time.ctime(int(meta_dict["timestamp"]))
     meta_dict["sequence"] = int(meta_dict["sequence"])
 
-    if topic == "heartbeat":
-        connect_time = int(meta_dict["connected"])
-        if connect_time == 0:
-            connect_str = "*not connected*"
-        else:
-            connect_str = time.ctime(connect_time)
-        line = "{0:30} {1:20} {2:8} connected={3}".format(
-            meta_dict["timestamp"], 
-            topic, 
-            meta_dict["sequence"], 
-            connect_str)
-    else:
-        table_name, raw_data = data.split("\n")
-        try:
-            update_data = \
-                pickle.loads(zlib.decompress(base64.b64decode(raw_data)))
-        except Exception:
-            log.exception("unable to parse data '{0}'".format(raw_data))
-            return 
-
-        line = "{0:30} {1:20} {2:8} {3}".format(
-            meta_dict["timestamp"], 
-            topic, 
-            meta_dict["sequence"], 
-            update_data)
-
-    log.info(line)
-
     if expected_sequence[topic] is None:
         expected_sequence[topic] = meta_dict["sequence"] + 1
     elif meta_dict["sequence"] == expected_sequence[topic]:
@@ -94,6 +113,21 @@ def _process_one_event(memcached_client, expected_sequence, topic, meta, data):
                 topic, expected_sequence[topic], meta_dict["sequence"])
         log.error(message)
         expected_sequence[topic] = None
+
+    table_name, raw_data = data.split("\n")
+    try:
+        event_data = \
+            pickle.loads(zlib.decompress(base64.b64decode(raw_data)))
+    except Exception:
+        log.exception("unable to parse {0} data '{1}'".format(
+            table_name, raw_data))
+        return 
+
+    if "table_name" not in event_data or \
+        event_data["table_name"] not in _dispatch_table:
+        log.error("invalid event data {0}".format(event_data))
+
+    _dispatch_table[event_data["table_name"]](memcached_client, event_data)
 
 def main():
     """
@@ -114,7 +148,6 @@ def main():
     sub_socket = _create_sub_socket(zeromq_context)
 
     expected_sequence = {
-        _heartbeat_channel    : None,
         _cache_update_channel : None,
     }
 

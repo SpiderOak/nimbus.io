e2a620c144568831ad96246ed764849bb21f2173
dougfort <dougfort@spideroak.com>
2012-09-25 17:24:29 -0400

    Ticket #50 When listing collections for a user, show whether a collection is a default collection.

--- web_collection_manager/list_collections_view.py
+++ web_collection_manager/list_collections_view.py
@@ -13,6 +13,7 @@ import flask
 from tools.greenlet_database_util import GetConnection
 from tools.data_definitions import http_timestamp_str
 from tools.customer_key_lookup import CustomerKeyConnectionLookup
+from tools.collection import compute_default_collection_name
 
 from web_collection_manager.connection_pool_view import ConnectionPoolView
 from web_collection_manager.authenticator import authenticate
@@ -26,7 +27,8 @@ def _list_collections(connection, customer_id):
     """
     cursor = connection.cursor()
     cursor.execute("""
-        select name, versioning, creation_time from nimbusio_central.collection   
+        select name, versioning, access_control, creation_time 
+        from nimbusio_central.collection   
         where customer_id = %s and deletion_time is null
         """, [customer_id, ])
     result = cursor.fetchall()
@@ -52,18 +54,29 @@ class ListCollectionsView(ConnectionPoolView):
             if customer_id is None:
                 flask.abort(httplib.UNAUTHORIZED)
 
-            collection_list = _list_collections(connection, customer_id)
+            raw_collection_list = _list_collections(connection, customer_id)
 
-        # json won't dump datetime
-        json_collections = [
-            {"name" : n, 
-             "versioning" : v, 
-             "creation-time" : http_timestamp_str(t)} \
-            for (n, v, t) in collection_list]
+        # ticket #50 When listing collections for a user, show whether a
+        # collection is a default collection.
+        default_collection_name = compute_default_collection_name(username)
+
+        collection_list = list()
+        for raw_entry in raw_collection_list:
+            name, versioning, raw_access_control, raw_creation_time = raw_entry
+            if raw_access_control is None:
+                access_control = None
+            else:
+                access_control = json.loads(raw_access_control)
+            entry = {"name" : name, 
+                     "default_collection" : name == default_collection_name,
+                     "versioning" : versioning, 
+                     "access_control" : access_control,
+                     "creation-time" : http_timestamp_str(raw_creation_time)}
+            collection_list.append(entry)
 
         # 2012-08-16 dougfort Ticket #28 - set content_type
         # 2012-08-16 dougfort Ticket #29 - format json for debuging
-        return flask.Response(json.dumps(json_collections, 
+        return flask.Response(json.dumps(collection_list, 
                                          sort_keys=True, 
                                          indent=4), 
                               status=httplib.OK,

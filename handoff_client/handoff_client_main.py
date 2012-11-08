6adc32d0d0663bf2af8a06e95a52f234d7283bb0
dougfort <dougfort@spideroak.com>
2012-11-08 17:03:47 -0500

    Ticket #63 handoff_client: start on database access

--- handoff_client/handoff_client_main.py
+++ handoff_client/handoff_client_main.py
@@ -7,77 +7,18 @@ handoff_client_main.py
 import logging
 import os
 import sys
-from threading import Event
 
 import zmq
 
 from tools.standard_logging import initialize_logging
-from tools.database_connection import get_node_connection
 from tools.event_push_client import EventPushClient, unhandled_exception_topic
+
+from handoff_client.get_node_ids import get_node_ids
+from handoff_client.get_handoff_rows import get_handoff_rows
 
 _local_node_name = os.environ["NIMBUSIO_NODE_NAME"]
 _log_path = "{0}/nimbusio_handoff_client_{1}.log".format(
     os.environ["NIMBUSIO_LOG_DIR"], _local_node_name,)
-_node_names = os.environ["NIMBUSIO_NODE_NAME_SEQ"].split()
-_node_database_hosts = \
-    os.environ["NIMBUSIO_NODE_DATABASE_HOSTS"].split()
-_node_database_ports = \
-    os.environ["NIMBUSIO_NODE_DATABASE_PORTS"].split()
-_node_database_passwords = \
-    os.environ["NIMBUSIO_NODE_USER_PASSWORDS"].split() 
-
-def _retrieve_conjoined_handoffs_for_node(interaction_pool, node_id):
-    query = """
-        select * from nimbusio_node.conjoined
-        where handoff_node_id = %s
-        order by unified_id
-    """
-    async_result = \
-        interaction_pool.run(interaction=query, 
-                             interaction_args=[node_id, ],
-                             pool=_local_node_name) 
-    result_list = async_result.get()
-
-    conjoined_row_list = list()
-    for row in result_list:
-        # bytea columns come out of the database as buffer objects
-        if row["combined_hash"] is not None: 
-            row["combined_hash"] = str(row["combined_hash"])
-        # row is of type psycopg2.extras.RealDictRow
-        # we want something we can pickle
-        conjoined_row_list.append(dict(row.items()))
-
-    return conjoined_row_list
-
-def _retrieve_segment_handoffs_for_node(interaction_pool, node_id):
-    query = """
-        select * from nimbusio_node.segment 
-        where handoff_node_id = %s
-        order by timestamp desc
-    """
-    async_result = \
-        interaction_pool.run(interaction=query, 
-                             interaction_args=[node_id, ],
-                             pool=_local_node_name) 
-    result_list = async_result.get()
-
-    segment_row_list = list()
-    for row in result_list:
-        # bytea columns come out of the database as buffer objects
-        if row["file_hash"] is not None: 
-            row["file_hash"] = str(row["file_hash"])
-        # row is of type psycopg2.extras.RealDictRow
-        # we want something we can pickle
-        segment_row_list.append(dict(row.items()))
-
-    return segment_row_list
-
-def _get_handoff_segments():
-    handoff_segments = list()
-    return handoff_segments
-
-def _process_handoff_segments(handoff_segments):
-    pass
 
 def main():
     """
@@ -95,9 +36,11 @@ def main():
 
     return_code = 0
     try:
-        handoff_segments = _get_handoff_segments()
-        if len(handoff_segments) > 0:
-            _process_handoff_segments(handoff_segments)
+        node_dict = get_node_ids(_local_node_name)
+        conjoined_rows, segment_rows = \
+            get_handoff_rows(node_dict[_local_node_name])
+#        if len(conjoined_rows) + len(segment_rows) > 0:
+#            _process_handoff_rows(conjoined_rows, segment_rows)
     except Exception as instance:
         log.exception("Uhandled exception {0}".format(instance))
         event_push_client.exception(

7431a1cc7de075e5c982e2bfcd1d601952d156ef
dougfort <dougfort@spideroak.com>
2012-11-21 09:05:44 -0500

    Ticket #63 handoff_client: clean up some database details after review

--- handoff_client/node_databases.py
+++ handoff_client/node_databases.py
@@ -6,7 +6,8 @@ connections to (remote) node databases
 """
 import os
 import psycopg2
-import psycopg2.extras
+import psycopg2.extensions
+from psycopg2.extras import RealDictConnection
 
 from tools.database_connection import get_node_database_dsn
 
@@ -22,6 +23,9 @@ def get_node_databases():
     """
     return a dict of database connections keyed by node name
     """
+    psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+    psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
     node_databases = dict()
     node_database_list = zip(_node_names, 
                              _node_database_hosts,
@@ -29,10 +33,11 @@ def get_node_databases():
                              _node_database_passwords)
 
     for name, host, port, password in node_database_list:
+
         # XXX: temporary expedient until boostrap is fixed
         host='localhost'
         dsn = get_node_database_dsn(name, password, host, port)
-        connection = psycopg2.connect(dsn)
+        connection = RealDictConnection(dsn)
         node_databases[name] = connection
 
     return node_databases       

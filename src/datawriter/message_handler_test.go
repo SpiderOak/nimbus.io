34ed659e4a21b7e9a164f825cbc8219c6bd00622
dougfort <dougfort@spideroak.com>
2014-06-12 09:13:23 -0400

    successful json.Unmarshall of large number

--- src/datawriter/message_handler_test.go
+++ src/datawriter/message_handler_test.go
@@ -9,42 +9,37 @@ python message dict
  'timestamp-repr': 'datetime.datetime(2014, 6, 11, 19, 8, 51, 574370)',
  'message-type': 'start-conjoined-archive',
  'collection-id': 5555, 'message-id': 'd33181e8f19b11e383cd08002708c001',
- 'user-request-id': 'fbcb114c-730c-410a-a356-d8de3ea02dd6'}
+ 'user-request-id': 'fbcb114c-730c-410a-a356-d8de3ea02dd6',
+ 'source-node-name': 'dc0-01-06'}
 */
 
 import (
 	"encoding/json"
 	"testing"
-
-	"datawriter/types"
 )
 
+type StartConjoinedArchive struct {
+	CollectionID    uint32 `json:"collection-id"`
+	Key             string `json:"key"`
+	UnifiedID       uint64 `json:"unified-id"`
+	HandoffNodeName string `json:"handoff-node-name"`
+}
+
 const (
-	testJSONString = `{"priority": 1402513731, "unified-id": 740661320822559745, "timestamp-repr": "datetime.datetime(2014, 6, 11, 19, 8, 51, 574370)", "key": "test_entire_multipart", "collection-id": 5555, "message-id": "d33181e8f19b11e383cd08002708c001", "message-type": "start-conjoined-archive", "user-request-id": "fbcb114c-730c-410a-a356-d8de3ea02dd6"}`
+	testJSONString = `{"priority": 1402513731, "unified-id": 740661320822559745, "timestamp-repr": "datetime.datetime(2014, 6, 11, 19, 8, 51, 574370)", "key": "test_entire_multipart", "collection-id": 5555, "message-id": "d33181e8f19b11e383cd08002708c001", "message-type": "start-conjoined-archive", "user-request-id": "fbcb114c-730c-410a-a356-d8de3ea02dd6", "source-node-name": "dc0-01-06"}`
 )
 
 func TestParsingJSONMessage(t *testing.T) {
-	var messageMap types.MessageMap
-	var err error
-	var ok bool
-	var floatUnifiedID float64
-	var unifiedID uint64
+	var startConjoinedArchive StartConjoinedArchive
 
-	err = json.Unmarshal([]byte(testJSONString), &messageMap)
+	err := json.Unmarshal([]byte(testJSONString), &startConjoinedArchive)
 	if err != nil {
 		t.Fatalf("json.Unmarshal failed %s", err)
 	}
 
-	if floatUnifiedID, ok = messageMap["unified-id"].(float64); !ok {
-		t.Fatalf("unparseable unified-id %T, %s", messageMap["unified-id"],
-			messageMap["unified-id"])
-	}
+	t.Logf("handoff node name = '%s'", startConjoinedArchive.HandoffNodeName)
 
-	t.Logf("floatUnifiedID =  %f", floatUnifiedID)
-
-	unifiedID = uint64(floatUnifiedID)
-
-	if unifiedID != uint64(740661320822559745) {
-		t.Fatalf("invalid unified id %d", unifiedID)
+	if startConjoinedArchive.UnifiedID != uint64(740661320822559745) {
+		t.Fatalf("invalid unified id %d", startConjoinedArchive.UnifiedID)
 	}
 }

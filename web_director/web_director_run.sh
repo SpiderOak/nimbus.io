dbd4b4f368e813011536f6f432aea271d6cddf26
dougfort <dougfort@spideroak.com>
2014-08-05 12:44:07 -0400

    save the web_director/web_director_run.sh that runs the go webdirector

--- web_director/web_director_run.sh
+++ web_director/web_director_run.sh
@@ -6,9 +6,9 @@ set -x
 export GOBIN="/home/pandora/nimbus.io/bin"
 export NIMBUSIO_WILDCARD_SSL_CERT="/home/pandora/keys/cacert.pem"
 export NIMBUSIO_WILDCARD_SSL_KEY="/home/pandora/keys/privkey.pem"
-export NIMBUSIO_WEB_DIRECTOR_ADDR="127.0.0.1"
+export NIMBUSIO_WEB_DIRECTOR_ADDR="dev.nimbus.io"
 export NIMBUSIO_WEB_DIRECTOR_PORT="9443"
-export NIMBUS_IO_SERVICE_DOMAIN="127.0.0.1"
+export NIMBUS_IO_SERVICE_DOMAIN="dev.nimbus.io"
 
 exec $GOBIN/webdirector &> /var/log/pandora/nimbus/webdirector.log
 

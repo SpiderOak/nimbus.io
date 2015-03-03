1be11af27201618d206fd4cf6f5ec807650f7695
dougfort <dougfort@spideroak.com>
2015-03-03 23:00:12 +0000

    start parsing request

--- src/webwriter/handler.go
+++ src/webwriter/handler.go
@@ -5,10 +5,6 @@ import (
 	"net/http"
 )
 
-var (
-	requestIDKey = http.CanonicalHeaderKey("x-nimbus-io-user-request-id")
-)
-
 type handlerStruct struct {
 }
 
@@ -20,21 +16,28 @@ func NewHandler() http.Handler {
 
 // ServeHTTP implements the http.Handler interface
 // handles all HTTP requests
+
+// https://<collection name>.nimbus.io/data/<key>
+// https://<collection name>.nimbus.io/data/<key>?action=delete
+// https://<collection name>.nimbus.io/conjoined/<key>?action=start
+// https://<collection name>.nimbus.io/conjoined/<key>?action=finish&conjoined_identifier=<conjoined_identifier>
+// https://<collection name>.nimbus.io/conjoined/<key>?action=abort&conjoined_identifier=<conjoined_identifier>
+
 func (h *handlerStruct) ServeHTTP(responseWriter http.ResponseWriter,
 	request *http.Request) {
 	var err error
+	var parsedRequest ParsedRequest
 
-	// Handle ping early so it doesn't clutter up the code
-	if request.Method == "GET" && request.URL.Path == "/ping" {
-		if _, err = responseWriter.Write([]byte("ok")); err != nil {
-			log.Printf("error: ping responseWriter.Write %s", err)
-		}
+	if parsedRequest, err = parseRequest(request); err != nil {
+		log.Printf("error: unparsable request: %s", err)
+		http.Error(responseWriter, "unparsable request", http.StatusBadRequest)
 		return
 	}
 
-	requestID := request.Header.Get(requestIDKey)
-	log.Printf("debug: %s method=%s, URL=%s from %s",
-		requestID, request.Method, request.URL, request.RemoteAddr)
+	log.Printf("debug: %s method=%s, collection=%s path=%s query=%s fragment=%s URI=%s %s",
+		parsedRequest.RequestID, request.Method, parsedRequest.CollectionName,
+		request.URL.Path, request.URL.RawQuery, request.URL.Fragment,
+		request.RequestURI, request.RemoteAddr)
 
 	http.NotFound(responseWriter, request)
 }
---
+++ src/webwriter/request_discriminator.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path"
+	"regexp"
+	"strings"
+)
+
+type RequestType uint8
+
+const (
+	_                         = iota
+	RespondToPing RequestType = iota
+	ArchiveKey
+	DeleteKey
+	StartConjoined
+	FinishConjoined
+	AbortConjoined
+)
+
+type ParsedRequest struct {
+	Type           RequestType
+	RequestID      string
+	CollectionName string
+	Key            string
+	UnifiedID      uint64
+}
+
+var (
+	requestIDKey          = http.CanonicalHeaderKey("x-nimbus-io-user-request-id")
+	validCollectionRegexp = regexp.MustCompile(`^[a-zA-Z0-9-]+$`)
+	validKeyRegexp        = regexp.MustCompile(`^\S+$`)
+)
+
+func parseRequest(request *http.Request) (ParsedRequest, error) {
+	var parsedRequest ParsedRequest
+	var err error
+
+	if request.Method == "GET" && request.URL.Path == "/ping" {
+		parsedRequest.Type = RespondToPing
+		return parsedRequest, nil
+	}
+
+	parsedRequest.RequestID = request.Header.Get(requestIDKey)
+
+	if parsedRequest.CollectionName, err = parseCollectionName(request.Host); err != nil {
+		return parsedRequest, fmt.Errorf("%s invalid collection %s %s",
+			parsedRequest.RequestID, request.Host, err)
+	}
+
+	pathDir, pathKey := path.Split(request.URL.Path)
+	if !validKeyRegexp.MatchString(pathKey) {
+		return parsedRequest, fmt.Errorf("%s invalid key %s",
+			parsedRequest.RequestID, request.URL.Path)
+	}
+
+	parsedRequest.Key = pathKey
+
+	switch pathDir {
+	case "data/":
+		if request.Method == "DELETE" {
+			parsedRequest.Type = DeleteKey
+			return parsedRequest, nil
+		}
+
+		if request.Method != "POST" {
+			return parsedRequest, fmt.Errorf("%s invalid method %s for path %s",
+				parsedRequest.RequestID, request.Method, request.URL.Path)
+		}
+
+		switch request.URL.Query().Get("action") {
+		case "":
+			parsedRequest.Type = ArchiveKey
+			return parsedRequest, nil
+		case "delete":
+			parsedRequest.Type = DeleteKey
+			return parsedRequest, nil
+		}
+
+	case "conjoined/":
+		if request.Method != "POST" {
+			return parsedRequest, fmt.Errorf("%s invalid method %s for path %s",
+				parsedRequest.RequestID, request.Method, request.URL.Path)
+		}
+
+		switch request.URL.Query().Get("action") {
+		case "start":
+			parsedRequest.Type = StartConjoined
+			return parsedRequest, nil
+		case "finish":
+			parsedRequest.Type = FinishConjoined
+			parsedRequest.UnifiedID, err = parseConjoinedIdentifier(
+				request.URL.Query().Get("conjoined_identifier"))
+			if err != nil {
+				return parsedRequest, fmt.Errorf("%s conjoined identifier %s %s",
+					parsedRequest.RequestID,
+					request.URL.Query().Get("conjoined_identifier"), err)
+			}
+			return parsedRequest, nil
+		case "abort":
+			parsedRequest.Type = AbortConjoined
+			parsedRequest.UnifiedID, err = parseConjoinedIdentifier(
+				request.URL.Query().Get("conjoined_identifier"))
+			if err != nil {
+				return parsedRequest, fmt.Errorf("%s conjoined identifier %s %s",
+					parsedRequest.RequestID,
+					request.URL.Query().Get("conjoined_identifier"), err)
+			}
+			return parsedRequest, nil
+		}
+
+	default:
+		return parsedRequest, fmt.Errorf("%s invalid path %s",
+			parsedRequest.RequestID, request.URL.Path)
+	}
+
+	return parsedRequest, fmt.Errorf("%s unparsable request; method %s; path %s",
+		parsedRequest.RequestID, request.Method, request.URL.Path)
+}
+
+func parseCollectionName(host string) (string, error) {
+	index := strings.Index(host, ".")
+	if index == -1 {
+		return "", fmt.Errorf("invalid hostname lacks '.' %s", host)
+	}
+	collectionName := host[:index]
+	if !validCollectionRegexp.MatchString(collectionName) {
+		return "", fmt.Errorf("invalid collection name %q", collectionName)
+	}
+
+	return collectionName, nil
+}
+
+func parseConjoinedIdentifier(publicIdentifier string) (uint64, error) {
+	return 0, fmt.Errorf("not implemented")
+}
